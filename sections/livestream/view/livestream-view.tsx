c7cf025ed7da73e4fc97c2b6c7827ac328fc7f62
Trương Công Trung <108384180+truongcongtrung2709@users.noreply.github.com>
2024-05-18 12:41:45 +0700

    feat(api): change api

--- sections/livestream/view/livestream-view.tsx
+++ sections/livestream/view/livestream-view.tsx
@@ -11,49 +11,20 @@ import { IMatchItem } from '#/types/match';
 import QueryString from 'qs';
 import { axiosSoccer } from '#/utils/axios';
 import { SOCCER_API } from '#/config-global';
+import { useGetMatches } from '#/api/match';
 
 
 // ----------------------------------------------------------------------
 
 export default function LivestreamView() {
   // const [currentPage, setCurrentPage] = useState(1);
-  const [matches, setMatches] = useState<IMatchItem[]>([]);
+  const { matches, matchesLoading, matchesEmpty } = useGetMatches();
 
-  // const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
-  //   setCurrentPage(page);
-  // };
-  // const { livestreams, livestreamsLoading, paginate, livestreamsEmpty } = useGetLivestreams(currentPage, 12)
-  // const sortedLivestreams = [...livestreams].sort((a, b) => {
-  //   return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-  // });
-  // useEffect(() => {
-  //   if (resposneData) {
-  //     setMatches(resposneData.data.list)
-  //   }
-  // }, [])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = QueryString.stringify({
-          'type': '1'
-        });
-        const response = await axiosSoccer.post(SOCCER_API as string, data);
-        // Handle success
-        setMatches(response.data.data.list);
-      } catch (error) {
-        // Handle error
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <Container style={{ maxWidth: "1330px", padding: "0" }}>
       <Typography sx={{ textTransform: "uppercase", my: 8 }} variant="h3">LIVESTREAM</Typography>
 
-      <MatchList matches={matches} />
+      <MatchList matches={matches} matchesLoading={matchesLoading} matchesEmpty={matchesEmpty} />
       {/* <Typography sx={{ textTransform: "uppercase", my: 8 }} variant="h3">LIVESTREAM KHÁC</Typography>
       <LivestreamList
         livestreams={sortedLivestreams}
